0ccb1216952596089822d5f29944adeb723485c2
dejhchai <dejhchai.n@gmail.com>
2018-12-20 23:38:57 +0700

    Add files via upload

--- bot-mea.php
+++ bot-mea.php
@@ -24,7 +24,7 @@ $arrayHeader = array();
    if($message == "สวัสดี"){
       $arrayPostData['to'] = $id;
       $arrayPostData['messages'][0]['type'] = "text";
-      $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา". $typ;
+      $arrayPostData['messages'][0]['text'] = $typ;
       $arrayPostData['messages'][1]['type'] = "sticker";
       $arrayPostData['messages'][1]['packageId'] = "2";
       $arrayPostData['messages'][1]['stickerId'] = "34";
