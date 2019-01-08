f7e465a48d68825d3f70a0eceb8ffe57259c3c20
dejhchai <dejhchai.n@gmail.com>
2019-01-08 23:00:34 +0700

    Add files via upload

--- rf-notify-webhook.php
+++ rf-notify-webhook.php
@@ -45,13 +45,32 @@ $myfile = fopen("webhook.txt", "w") or die("Unable to open file!");
 //$str=print_r($_GET,true);
 //fwrite($myfile, $str);
 fwrite($myfile, $content);
-fwrite($myfile, "1>");
-fwrite($myfile, $events['title']);
-fwrite($myfile, "2>");
-fwrite($myfile, $events['evalMatches']['title']);
+//fwrite($myfile, $events['title']);
 //fwrite($myfile, $value."\r\n");
 fclose($myfile);
 //$content = print_r($yourVar, true);
 //file_put_contents('file.log', $content);
 
+
+$token = "wxTVZE2k6DLTzwjwYnqFXx1MvcefwAD6LIt9gPfEDZT"; //ใส่Token ที่copy เอาไว้
+$message = "\r\n". $events['title']."\r\n[". $events[0]['value']."]"; //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
+ 
+//$res = notify_message($str,$token);
+//print_r($res);
+$queryData = array('message' => $message);
+ $queryData = http_build_query($queryData,'','&');
+ $headerOptions = array( 
+         'http'=>array(
+            'method'=>'POST',
+            'header'=> "Content-Type: application/x-www-form-urlencoded\r\n"
+                      ."Authorization: Bearer ".$token."\r\n"
+                      ."Content-Length: ".strlen($queryData)."\r\n",
+            'content' => $queryData
+         ),
+ );
+ $context = stream_context_create($headerOptions);
+ $result = file_get_contents("https://notify-api.line.me/api/notify",FALSE,$context);
+ $res = json_decode($result);
+ print_r($res);
+ 
 ?>
