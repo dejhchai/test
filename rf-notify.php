f8da89f20491025f712b9655910bd45a06fe8ffa
dejhchai <dejhchai.n@gmail.com>
2019-01-08 19:26:25 +0700

    Add files via upload

--- rf-notify.php
+++ rf-notify.php
@@ -1,12 +1,25 @@
 <?php
 
 define('LINE_API',"https://notify-api.line.me/api/notify");
- 
+
+if(isset($_POST["msg"]) === true && empty($_POST["msg"]) === false)
+{	$msg=$_POST["msg"];
+}else if(isset($_GET["msg"]) === true && empty($_GET["msg"]) === false)
+{       $msg=$_GET["msg"];
+}else
+{    	$msg="";
+}
+
+ if($msg!="")
+ {
 $token = "wmuZn7ixkOkAp2V3WWLxr0xBJwFxKItscY5iuFLfdo7"; //ใส่Token ที่copy เอาไว้
-$str = "Hello"; //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
+$str = $cmd; //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
  
 $res = notify_message($str,$token);
 print_r($res);
+
+}
+
 function notify_message($message,$token){
  $queryData = array('message' => $message);
  $queryData = http_build_query($queryData,'','&');
@@ -24,5 +37,4 @@ function notify_message($message,$token){
  $res = json_decode($result);
  return $res;
 }
-
 ?>
