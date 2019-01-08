50593de47664146e0df6111a6908b4f3c5717d65
dejhchai <dejhchai.n@gmail.com>
2019-01-08 22:10:59 +0700

    Add files via upload

--- rf-notify-webhook.php
+++ rf-notify-webhook.php
@@ -1,12 +1,14 @@
 <?php
+$content = file_get_contents('php://input');
+
 $myfile = fopen("webhook.txt", "w") or die("Unable to open file!");
-$str=print_r($_POST,true);
-fwrite($myfile, $str);
-$str=print_r($_GET,true);
-fwrite($myfile, $str);
+//$str=print_r($_POST,true);
+//fwrite($myfile, $str);
+//$str=print_r($_GET,true);
+//fwrite($myfile, $str);
+fwrite($myfile, $content);
 fclose($myfile);
 //$content = print_r($yourVar, true);
 //file_put_contents('file.log', $content);
-print_r($_POST);
-print_r($_GET);
+
 ?>
